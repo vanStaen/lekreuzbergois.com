d736bc993108f039b8a572ff2be9393d3d68de61
vanStaen <clement.vanstaen@gmail.com>
2023-08-23 10:20:16 +0200

    Move gallery logic in own component

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,136 @@
+import React, { useState, useEffect, useRef } from "react";
+import { observer } from "mobx-react-lite";
+import { LoadingOutlined } from "@ant-design/icons";
+import { useTranslation } from "react-i18next";
+import { Spin } from "antd";
+
+import { pageStore } from "../../store/pageStore";
+import { pictureStore } from "../../store/pictureStore";
+import { Overlay } from "../Overlay/Overlay";
+import useImage from "../../helpers/useImage";
+
+import './Gallery.less';
+
+export const Gallery = observer(() => {
+    const [imageDesc, setImageDesc] = useState(false);
+    const [showOverlay, setShowOverlay] = useState(false);
+    const { t } = useTranslation();
+
+    const elementForCursor = document.getElementById("image-container");
+    const track = document.getElementById("image-track");
+    const mouseDownAt = useRef(0);
+    const prevPercentage = useRef(0);
+    const percentage = useRef(0);
+
+    const handleOnDown = (e) => {
+        mouseDownAt.current = e.clientX;
+        elementForCursor.style.cursor = "grabbing";
+    };
+
+    const handleOnUp = () => {
+        mouseDownAt.current = 0;
+        prevPercentage.current = percentage.current;
+        elementForCursor.style.cursor = "grab";
+    };
+
+    const handleOnMove = (e) => {
+        if (mouseDownAt.current === 0) return;
+
+        const mouseDelta = parseFloat(mouseDownAt.current) - e.clientX,
+            maxDelta = window.innerWidth / 2;
+
+        const result = (mouseDelta / maxDelta) * -100,
+            nextPercentageUnconstrained = parseFloat(prevPercentage.current) + result,
+            nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+        percentage.current = nextPercentage;
+
+        track.animate(
+            {
+                transform: `translate(${nextPercentage}%, -50%)`,
+            },
+            { duration: 1200, fill: "forwards" }
+        );
+
+        for (const image of track.getElementsByClassName("image")) {
+            image.animate(
+                {
+                    objectPosition: `${100 + nextPercentage}% center`,
+                },
+                { duration: 1200, fill: "forwards" }
+            );
+        }
+    };
+
+
+    const handleMouseOver = (desc, year) => {
+        const formatedDesc = (
+            <div>
+                {desc}
+                <span className="imageDescriptionSeparator"> | </span>
+                {year}
+                <div className="imageDescriptionInfoOpen">
+                    {t("gallery.doubleClickToExpend")}
+                </div>
+            </div>
+        );
+        setImageDesc(formatedDesc);
+    };
+
+    window.onmousedown = (e) => handleOnDown(e);
+    window.onmouseup = (e) => handleOnUp(e);
+    window.onmousemove = (e) => handleOnMove(e);
+
+    const handleMouseLeave = () => {
+        setImageDesc(null);
+    };
+
+    const handleDoubleClick = async (pictureId) => {
+        await pictureStore.setSelectedPicture(pictureId);
+        setShowOverlay(true);
+    };
+
+    const picturesFormatted = pictureStore.pictures.map((picture) => {
+        const { loading, image } = useImage(
+            picture.name,
+            "img/thumbs",
+            picture.imgtype
+        );
+
+        return (
+            <div
+                className="imageContainer"
+                onMouseOver={() => handleMouseOver(picture.desc, picture.year)}
+                onMouseLeave={handleMouseLeave}
+                onDoubleClick={() => handleDoubleClick(picture.id)}
+            >
+                {loading ? (
+                    <Spin
+                        indicator={<LoadingOutlined spin />}
+                        size="large"
+                        className="imageLoading"
+                    />
+                ) : (
+                    <img
+                        className={`image ${!pageStore.showSensiblePictures &&
+                            picture.explicit &&
+                            "imageSensible"
+                            }`}
+                        src={image}
+                        draggable="false"
+                    />
+                )}
+            </div>
+        );
+    });
+
+    return (
+        <>
+            <div className="gallery__imageContainer" id="image-container">
+                <div id="image-track">{picturesFormatted}</div>
+            </div>
+            {showOverlay && <Overlay setShowOverlay={setShowOverlay} />}
+            {imageDesc && <div className="gallery__imageDescription">{imageDesc}</div>}
+        </>)
+        ;
+});
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,82 +1,19 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { LoadingOutlined } from "@ant-design/icons";
 import { observer } from "mobx-react-lite";
-import { Spin } from "antd";
 
 import { ShowSensibleSlider } from "../components/ShowSensibleSlider/ShowSensibleSlider";
-import { pageStore } from "../store/pageStore";
-import { pictureStore } from "../store/pictureStore";
-import { Overlay } from "../components/Overlay/Overlay";
 import { Title } from "../components/Title/Title";
-
-import useImage from "../helpers/useImage";
+import { Gallery } from "../components/Gallery/Gallery";
 
 import "./Main.less";
 
 export const Main = observer(() => {
-  const [imageDesc, setImageDesc] = useState(false);
-  const [showOverlay, setShowOverlay] = useState(false);
-  const track = document.getElementById("image-track");
-  const mouseDownAt = useRef(0);
-  const prevPercentage = useRef(0);
-  const percentage = useRef(0);
-  const { t } = useTranslation();
-
-  const elementForCursor = document.getElementById("image-container");
-
-  const handleOnDown = (e) => {
-    mouseDownAt.current = e.clientX;
-    elementForCursor.style.cursor = "grabbing";
-  };
-
-  const handleOnUp = () => {
-    mouseDownAt.current = 0;
-    prevPercentage.current = percentage.current;
-    elementForCursor.style.cursor = "grab";
-  };
-
-  const handleOnMove = (e) => {
-    if (mouseDownAt.current === 0) return;
-
-    const mouseDelta = parseFloat(mouseDownAt.current) - e.clientX,
-      maxDelta = window.innerWidth / 2;
-
-    const result = (mouseDelta / maxDelta) * -100,
-      nextPercentageUnconstrained = parseFloat(prevPercentage.current) + result,
-      nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-
-    percentage.current = nextPercentage;
-
-    track.animate(
-      {
-        transform: `translate(${nextPercentage}%, -50%)`,
-      },
-      { duration: 1200, fill: "forwards" }
-    );
-
-    for (const image of track.getElementsByClassName("image")) {
-      image.animate(
-        {
-          objectPosition: `${100 + nextPercentage}% center`,
-        },
-        { duration: 1200, fill: "forwards" }
-      );
-    }
-  };
 
   const handleOnScroll = (e) => {
     //TODO
     //console.log(e.deltaX);
   };
-
-  window.onmousedown = (e) => handleOnDown(e);
-  window.onmouseup = (e) => handleOnUp(e);
-  window.onmousemove = (e) => handleOnMove(e);
-  //TODO
-  //window.ontouchstart = (e) => handleOnDown(e.touches[0]);
-  //window.ontouchend = (e) => handleOnUp(e.touches[0]);
-  //window.ontouchmove = (e) => handleOnMove(e.touches[0]);
 
   useEffect(() => {
     document.addEventListener("wheel", handleOnScroll);
@@ -85,73 +22,12 @@ export const Main = observer(() => {
     };
   }, [handleOnScroll]);
 
-  const handleMouseOver = (desc, year) => {
-    const formatedDesc = (
-      <div>
-        {desc}
-        <span className="imageDescriptionSeparator"> | </span>
-        {year}
-        <div className="imageDescriptionInfoOpen">
-          {t("gallery.doubleClickToExpend")}
-        </div>
-      </div>
-    );
-    setImageDesc(formatedDesc);
-  };
-
-  const handleMouseLeave = () => {
-    setImageDesc(null);
-  };
-
-  const handleDoubleClick = async (pictureId) => {
-    await pictureStore.setSelectedPicture(pictureId);
-    setShowOverlay(true);
-  };
-
-  const picturesFormatted = pictureStore.pictures.map((picture) => {
-    const { loading, image } = useImage(
-      picture.name,
-      "img/thumbs",
-      picture.imgtype
-    );
-
-    return (
-      <div
-        className="imageContainer"
-        onMouseOver={() => handleMouseOver(picture.desc, picture.year)}
-        onMouseLeave={handleMouseLeave}
-        onDoubleClick={() => handleDoubleClick(picture.id)}
-      >
-        {loading ? (
-          <Spin
-            indicator={<LoadingOutlined spin />}
-            size="large"
-            className="imageLoading"
-          />
-        ) : (
-          <img
-            className={`image ${!pageStore.showSensiblePictures &&
-              picture.explicit &&
-              "imageSensible"
-              }`}
-            src={image}
-            draggable="false"
-          />
-        )}
-      </div>
-    );
-  });
-
   return (
     <div className="mainContainer" id="mainContainer">
       <div className="noisyBackGround" id="noisyBackGround"></div>
       {/*<Title showOverlay={showOverlay} />*/}
-      {showOverlay && <Overlay setShowOverlay={setShowOverlay} />}
       <ShowSensibleSlider />
-      <div className="image-container" id="image-container">
-        <div id="image-track">{picturesFormatted}</div>
-      </div>
-      {imageDesc && <div className="imageDescription">{imageDesc}</div>}
+      <Gallery />
     </div>
   );
 });
