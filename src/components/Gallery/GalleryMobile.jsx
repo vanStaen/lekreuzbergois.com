2171fce1af946865a9e9bb2c7b1f4eafa700e384
vanStaen <clement.vanstaen@gmail.com>
2023-08-23 11:34:03 +0200

    not done yet Mobile

--- src/components/Gallery/GalleryMobile.jsx
+++ src/components/Gallery/GalleryMobile.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { observer } from "mobx-react-lite";
+
+import { pageStore } from "../../store/pageStore";
 
 import './Gallery.less';
 
-export const GalleryMobile = () => {
+export const GalleryMobile = observer(() => {
     return (
         <div className="galleryMobile__container">
-            <div><b>Under construction.</b></div>
-            <div>Check this page on a laptop for now.</div>
+            <div>The mobile {pageStore.showSensiblePictures && 'explcit '}version is not done yet,</div>
+            <div>check this page on a laptop for now.</div>
         </div>
     );
-} 
+});
