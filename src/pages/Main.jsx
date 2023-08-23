520bc4eca4a40ac229dc614f2cab640e6642e354
vanStaen <clement.vanstaen@gmail.com>
2023-08-23 10:45:42 +0200

    gallery Mobile WIP

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,14 +1,26 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useTranslation } from "react-i18next";
+import React, { useEffect, useState } from "react";
 import { observer } from "mobx-react-lite";
 
 import { ShowSensibleSlider } from "../components/ShowSensibleSlider/ShowSensibleSlider";
 import { Title } from "../components/Title/Title";
 import { Gallery } from "../components/Gallery/Gallery";
+import { GalleryMobile } from "../components/Gallery/GalleryMobile";
+import { isMobileCheck } from "../helpers/checkMobileTablet";
 
-import "./Main.less";
+import "./Main.less"
+
+const THRESHOLD_IN_PX_BEFORE_SHOWING_MOBILE_GALLERY = 690;
 
 export const Main = observer(() => {
+  const [showMobileVersion, setShowMobileVersion] = useState(isMobileCheck());
+
+  const checkIfSmallScreen = () => {
+    if (window.innerWidth < THRESHOLD_IN_PX_BEFORE_SHOWING_MOBILE_GALLERY || isMobileCheck()) {
+      setShowMobileVersion(true);
+    } else {
+      setShowMobileVersion(false);
+    }
+  };
 
   const handleOnScroll = (e) => {
     //TODO
@@ -16,18 +28,21 @@ export const Main = observer(() => {
   };
 
   useEffect(() => {
+    window.addEventListener("resize", checkIfSmallScreen);
     document.addEventListener("wheel", handleOnScroll);
     return () => {
+      window.removeEventListener('resize', checkIfSmallScreen);
       document.removeEventListener("wheel", handleOnScroll);
     };
-  }, [handleOnScroll]);
+  }, [checkIfSmallScreen, handleOnScroll])
+
 
   return (
     <div className="mainContainer" id="mainContainer">
       <div className="noisyBackGround" id="noisyBackGround"></div>
       {/*<Title showOverlay={showOverlay} />*/}
       <ShowSensibleSlider />
-      <Gallery />
+      {showMobileVersion ? <GalleryMobile /> : <Gallery />}
     </div>
   );
 });
